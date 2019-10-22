1aeab57653739949affb2037f0981944c9fbe87b
Fred <thecanuckatheist@gmail.com>
2019-10-22 18:15:49 +0000

    survey.js final version with Leo

---
+++ survey.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What\'s your name? Nicknames are also acceptable :) ', (nick) => {
+  rl.question('What\'s an activity you like doing? ', (activity) => {
+    rl.question('What do you listen to while doing that? ', (music) => {
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (meal) => {
+        rl.question('What\'s your favourite thing to eat for that meal? ', (food) => {
+          rl.question('Which sport is your absolute favourite? ', (sport) => {
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (power) => {
+              console.log(`Thank you for your valuable feedback: ${nick} ${activity} ${music} ${meal} ${food} ${sport} ${power}`);
+
+              rl.close();
+            })
+          })
+        })
+      })
+    })
+  })
+});
